7511c7a564a14a859a2934f10c04a8bfe2da9ff1
arup saha <arup.saha@indusnet.co.in>
2021-09-29 14:03:11 +0530

    api service added

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+}
